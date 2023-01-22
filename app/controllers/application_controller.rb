1da0e6a50667fd1cd285f2de3d33749ded119d0a
Warren Liow <warrenliow@uchicago.edu>
2023-01-22 08:40:43 +0000

    GOod progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb"})
+  end
+
+  def calculate_square
+    @num = params.fetch("number").to_f
+    @square_of_num = @num * @num
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_random_form
+    render({ :template => "calculation_templates/random_form.html.erb"})
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @random_number = rand(@lower..@upper)
+    render({ :template => "calculation_templates/random_results.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+  get("/square/new", { :controller => "application", :action => "blank_square_form"})
+  get("/square/results", { :controller => "application", :action => "calculate_square"})
+
+  get("/random/new", { :controller => "application", :action => "blank_random_form"})
+  get("/random/results", { :controller => "application", :action => "calculate_random"})
+
 end
